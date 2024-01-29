0b1fcaea42ed5dac108dec11ce63177185d715c7
Mohamed Abdelazim <mabdelazemahmed@gmail.com>
2024-01-29 14:33:47 +0200

    solving some problems

--- BasicDS/DoublyLinkedList.py
+++ BasicDS/DoublyLinkedList.py
@@ -4,7 +4,61 @@ class Node:
         self.next = None
         self.prev = None
 
-# Doubly Linked List Structure
-class DLinkedList:
-    pass
-    # Under Construction
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insertNode(self, data):
+        newNode = Node(data)
+
+        if self.head is None:
+            self.head = newNode
+        else:
+            currentNode = self.head
+            while currentNode.next:
+                currentNode = currentNode.next
+            currentNode.next = newNode
+            newNode.prev = currentNode
+
+    def deleteNode(self, data):
+        if self.head is None:
+            return
+
+        currentNode = self.head
+        if currentNode.data == data:
+            self.head = currentNode.next
+            if self.head:
+                self.head.prev = None
+            return
+
+        while currentNode.next:
+            if currentNode.next.data == data:
+                currentNode.next = currentNode.next.next
+                if currentNode.next:
+                    currentNode.next.prev = currentNode
+                return
+            currentNode = currentNode.next
+
+    def findNode(self, nodeData):
+        currentNode = self.head
+
+        while currentNode:
+            if currentNode.data == nodeData:
+                return currentNode
+            currentNode = currentNode.next
+
+    def displayList(self):
+        currentNode = self.head
+
+        while currentNode:
+            print(currentNode.data, end=" <-> ")
+            currentNode = currentNode.next
+        print("None")
+
+# Test Area
+myList = DoublyLinkedList()
+myList.insertNode(1)
+myList.insertNode(2)
+myList.insertNode(3)
+myList.deleteNode(2)
+myList.displayList()
---
+++ Problems/twoSum.py
@@ -0,0 +1,26 @@
+# Brute Force
+# O(n^2)
+# def twoSum(array, target):
+#     for i in range(len(array)):
+#         for j in range(i + 1, len(array)):
+#             if array[i] + array[j] == target:
+#                 return [i, j]
+#     return None
+
+# O(n)
+# One Pass Hash
+def twoSum(array, target):
+    numMap, n = {}, len(array)
+
+    for i in range(n):
+        complement = target - array[i]
+
+        if complement in numMap:
+            return numMap
+
+        numMap[array[i]] = i
+
+    
+print(twoSum([2,7,11,5], 9))
+# print(twoSum([3,2,4], 6))
+# print(twoSum([3,3], 6))
